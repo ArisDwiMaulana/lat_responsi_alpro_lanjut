2f9e82eb28d4ae0733db62399a35408669cfc641
Aris Dwi Maulana <dwiaris3765@gmail.com>
2026-05-20 20:28:24 +0700

    menambahkan program input beserta validasi berdaasrkan NIM agar tidak ada duplikasi

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 using namespace std;
+
+struct Mahasiswa{
+    int NIM;
+    string Nama;
+    string JenisKelamin;
+    string TanggalLahir; // format : tanggal bulan tahun contoh (20 Mei 2006)
+    int Semester;
+    string Prodi;
+    string Fakultas;
+    double IPK;
+};
 
 void subMenuOutput(){
     int pilihan;
@@ -34,7 +47,64 @@ void subMenuOutput(){
         
     }while(pilihan != 3);
 }
+bool cek_nim_duplikat(int nim){
+    ifstream file("src/data.csv");
+    if(file.fail()){
+        cout<<"Gagal membuka file!"<<endl;
+        return false;
+    }
+    string line;
+    while(getline(file, line)){
+        stringstream ss(line);
+        string nim_pada_file;
+        getline(ss, nim_pada_file, ','); // Ambil NIM
+        if(stoi(nim_pada_file) == nim){ // cek apakah ada NIM yang sama
+            file.close();
+            return true; 
+        }
+    }
+    file.close();
+    return false; // NIM tidak ditemukan, tidak duplikat
+}
 
+void input_data(){
+    ofstream file("src/data.csv", ios::app);
+    if(file.fail()){
+        cout<<"Gagal membuka file!"<<endl;
+        return;
+    }
+    Mahasiswa temp;
+    bool duplikasi_nim = false;
+    do{
+        cout<<"Masukkan NIM : ";
+        cin>>temp.NIM;
+        duplikasi_nim = cek_nim_duplikat(temp.NIM);
+        if(duplikasi_nim){
+            system("cls");
+            cout<<"NIM sudah ada, silakan masukkan NIM yang berbeda!"<<endl;
+            system("pause");
+        }
+    }while(duplikasi_nim);
+    cin.ignore();
+    cout<<"Masukkan Nama : ";
+    getline(cin, temp.Nama);
+    cout<<"Masukkan Jenis Kelamin (Laki-Laki/Perempuan): ";
+    getline(cin, temp.JenisKelamin);
+    cout<<"Masukkan Tanggal Lahir (20 Mei 2006): ";
+    getline(cin, temp.TanggalLahir);
+    cout<<"Masukkan Semester : ";
+    cin>>temp.Semester;
+    cin.ignore();
+    cout<<"Masukkan Program Studi : ";
+    getline(cin, temp.Prodi);
+    cout<<"Masukkan Fakultas : ";
+    getline(cin, temp.Fakultas);
+    cout<<"Masukkan IPK : ";
+    cin>>temp.IPK;
+    file<<temp.NIM<<","<<temp.Nama<<","<<temp.JenisKelamin<<","<<temp.TanggalLahir<<","<<temp.Semester<<","<<temp.Prodi<<","<<temp.Fakultas<<","<<temp.IPK<<endl;   
+    file.close();
+    cout<<"Data berhasil disimpan!"<<endl;
+}
 int main() {
     int pilihan;
     do{
@@ -49,6 +119,7 @@ int main() {
             case 1: 
                 system("cls");
                 cout<<"Input data"<<endl;
+                input_data();
                 system("pause");
                 break;
             case 2:
